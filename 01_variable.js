676962ab7383ba8ad3be6c75cda61e9b337b5a1e
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-19 01:29:35 -0700

    ibrar29

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail= "ibrar@gmail.com"
+var accountPassword = "2345"
+accountCity = "Karachi"
+let accountState;
+
+
+accountEmail = "bilal@gmail.com"
+accountPassword = "6578"
+accountCity = "lahore";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// console.log(accountId);
---
+++ 02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+let name = "ibrar"
+let age = 34;
+let isLoggedIn =false
+
+
+//premitive datatype
+//symbol=> identify uniqueness
+Number
+String
+Boolean
+null
+undefined
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
