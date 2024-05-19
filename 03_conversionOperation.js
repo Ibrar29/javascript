4a747dd95e6433a3a5533edf993b07c5d7229fd1
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-19 03:40:39 -0700

    first commit

---
+++ 03_conversionOperation.js
@@ -0,0 +1,50 @@
+//let score ="33eed";
+//let score =null;
+// let score =undefined;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+//to string convert in number and check a number
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// "33"=> 33
+// "33tr"=>NaN
+
+//number to string convert
+let isLoggedIn ="ibrar"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+ let score = "ibrar"
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+//  console.log(typeof stringNumber);
+//  console.log (stringNumber)
+
+ // ****************************Operation*****************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+let str1 = "ibrar"
+let str2 = " arain"
+let str3 = str1 + str2
+//console.log(str3);
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+ // postfix and prefix
+// let x = 3;
+// const y = x++;
+// console.log(`x:${x}, y:${y}`);
+
+// let a =5
+// const b =++a
+// console.log(`a:${a}, b:${b}`);
