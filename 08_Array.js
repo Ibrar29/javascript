3dacd372a73276fc85f13307a17c9de5b39ad843
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-20 22:53:58 -0700

    Array

---
+++ 08_Array.js
@@ -0,0 +1,31 @@
+const myArr = [1,2,3,4,5,true,"ibrar"]
+//const myHeros = ["ibrar", "bilal","saad"]
+
+const newArr2 = new Array(1,2,3,4)
+//console.log(myArr[3]);
+// Array method
+
+// myArr.push(8)
+// //myArr.pop();
+//myArr.unshift(0)
+//myArr.shift()
+//console.log(myArr.includes(7));
+// //console.log(myArr.indexOf(5));  
+// const newArr = myArr.join()
+// console.log(myArr);
+
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myArr);
+console.log(myn2);
+
