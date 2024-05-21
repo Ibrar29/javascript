5426f09758cfeb51b58cb0d10d1d359e95fa544d
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-20 22:50:59 -0700

    dates in js

---
+++ 07_dates_in_js.js
@@ -0,0 +1,17 @@
+// Dates
+let myDate = new Date ()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedate = new Date (2024,5,6,5,8)
+console.log(myCreatedate.toLocaleString());
+
+
+let newDate = new Date()
+console.log(newDate.getMonth()+ 1);
+console.log(newDate.getDay());
+
+
